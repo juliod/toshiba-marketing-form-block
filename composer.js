704b9e34d89cf4ea2d7343901beafdf6236e4b4d
deypro <juliodiaz.jotas@gmail.com>
2024-07-17 10:28:40 +0200

    adding Composer file for unit testing

---
+++ composer.js
@@ -0,0 +1,20 @@
+{
+  "name": "deypro/toshiba-amketing-form-block",
+  "description": "This project is for wordpress bloc for Toshiba MArketing",
+  "minimum-stability": "stable",
+  "license": "MIT 2.0",
+  "authors": [{
+      "name": "deypro",
+      "email": "info@deypro.com"
+    }
+  ],
+  "autoload": {
+    "classmap": [
+      "src/"
+    ]
+  },
+  "require-dev": {
+    "phpunit/phpunit": "^9",
+    "phpunit/dbunit": "^4.0"
+  }
+}
