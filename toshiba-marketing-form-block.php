d43353352de4837f760af930565e5422f88f805d
jcdd <julio-cesar.diaz@elecnor.es>
2024-07-17 14:38:44 +0200

    Removed base errors

--- toshiba-marketing-form-block.php
+++ toshiba-marketing-form-block.php
@@ -6,34 +6,34 @@
  * Author: Your Name
  */
 
-function simple_form_block_register_block() {
+function toshiba_marketing_form_block_register_block() {
     // Register block editor script
     wp_register_script(
-        'simple-form-block-editor-script',
+        'toshiba-marketing-form-block-editor-script',
         plugins_url( 'dist/index.js', __FILE__ ),
         array( 'wp-blocks', 'wp-element', 'wp-editor', 'wp-components', 'wp-i18n' )
     );
 
     // Register block editor styles
     wp_register_style(
-        'simple-form-block-editor-style',
+        'toshiba-marketing-form-block-editor-style',
         plugins_url( 'src/editor.css', __FILE__ ),
         array( 'wp-edit-blocks' )
     );
 
     // Register block frontend styles
     wp_register_style(
-        'simple-form-block-style',
+        'toshiba-marketing-form-block-style',
         plugins_url( 'src/style.css', __FILE__ ),
         array()
     );
 
     // Register block
-    register_block_type( 'simple-form-block/form', array(
-        'editor_script' => 'simple-form-block-editor-script',
-        'editor_style' => 'simple-form-block-editor-style',
-        'style' => 'simple-form-block-style',
+    register_block_type( 'toshiba-marketing-form-block/form', array(
+        'editor_script' => 'toshiba-marketing-form-block-editor-script',
+        'editor_style' => 'toshiba-marketing-form-block-editor-style',
+        'style' => 'toshiba-marketing-form-block-style',
     ) );
 }
 
-add_action( 'init', 'simple_form_block_register_block' );
+add_action( 'init', 'toshiba_marketing_form_block_register_block' );
