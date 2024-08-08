45f50d8cf9deece370488d298ba3bfa7ff0d5286
jcdd <julio-cesar.diaz@elecnor.es>
2024-08-08 15:04:08 +0200

    Blocks para Toshiba con los campos que esperamos falta el action

--- src/render.php
+++ src/render.php
@@ -7,8 +7,7 @@
 	<?php esc_html_e( 'Toshiba Marketing Form – hello from a dynamic block!', 'toshiba-marketing-form-block' ); ?>
 </p>
 
-<!--
-<form id="wptoshibamarketing-form"  method="post" class="wptoshibamarketing-forms" enctype="multipart/form-data">
+<form id="wp-toshiba-marketinblock-form"  method="post" class="toshibamarketing-forms" action= enctype="multipart/form-data">
     <h3>Quiero información de este Toshiba</h3>
     <fieldset>
         <div class="clearfix">
