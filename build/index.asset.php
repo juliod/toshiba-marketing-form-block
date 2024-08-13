e5fd9f9c4c6d3bcf9469b59d3bddaccf09cdc311
jcdd <julio-cesar.diaz@elecnor.es>
2024-08-13 14:52:32 +0200

    first run

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '954c7c061bc4b4195897');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '0e2c4ec2e7f69fb596b2');
